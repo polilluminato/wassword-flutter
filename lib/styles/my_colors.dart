a6227133f144f836891bdb6bb732575c2441879d
Alberto Bonacina <bonacina.alberto@gmail.com>
2019-08-31 16:12:28 +0200

    Modificato tema della app

--- lib/styles/my_colors.dart
+++ lib/styles/my_colors.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 Color colorWhite = const Color(0xFFFFFFFF);
 Color colorRed = const Color(0xFFFF0000);
 Color colorMyBlue = const Color(0xFF1976D2);
+Color colorAccent = colorMyBlue;
 Color colorLightGrey = const Color(0xFFE0E0E0);
 
-Color colorGreyLight = const Color(0xFF4a5568);
-Color colorGreyDark = const Color(0xFF2d3848);
+Color colorGreyLight = const Color(0xFF37383b);
+Color colorGreyDark = const Color(0xFF202124);
 
-/*Colori*/
-Color colorTransparent = const Color(0x00FFFFFF);
-Color colorPrimary = const Color(0xFF757575);
-Color colorPrimaryDark = const Color(0xFF616161);
-Color colorAccent = colorMyBlue;
 /*Colori Testo*/
 Color colorPrimaryText = const Color(0xFF212121);
 Color colorPrimaryTextLight = const Color(0xFF757575);
