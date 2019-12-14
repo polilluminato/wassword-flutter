57916bc1f8a02856024afceb7f23d80a4879b53e
Alberto Bonacina <bonacina.alberto@gmail.com>
2019-12-14 15:28:31 +0100

    Removed unused color

--- lib/styles/my_colors.dart
+++ lib/styles/my_colors.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 
 Color colorWhite = const Color(0xFFFFFFFF);
-Color colorRed = const Color(0xFFFF0000);
 Color colorMyBlue = const Color(0xFF1976D2);
 Color colorAccent = colorMyBlue;
 Color colorLightGrey = const Color(0xFFE0E0E0);
 
 Color colorGreyLight = const Color(0xFF37383b);
 Color colorGreyDark = const Color(0xFF202124);
-
-/*Colori Testo*/
-Color colorPrimaryText = const Color(0xFF212121);
-Color colorPrimaryTextLight = const Color(0xFF757575);
-Color colorSecondaryText = const Color(0xFF757575);
-Color colorDivider = const Color(0xFFBDBDBD);
