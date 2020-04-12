febfe4461186d9e556a7d2254d50750d69bf525e
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-04-12 14:55:28 +0200

    New color for the app
    Update the app with new colors, recreate all the icon assets and splash screen

--- lib/styles/my_colors.dart
+++ lib/styles/my_colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 Color colorWhite = const Color(0xFFFFFFFF);
 Color colorLightGrey = const Color(0xFFE0E0E0);
 
-Color colorGreyLight = const Color(0xFF525356);
-Color colorGreyDark = const Color(0xFF08090b);
+Color colorGreyLight = const Color(0xFF585A5E);
+Color colorGreyDark = const Color(0xFF050607);
 
-Color colorAccentDark = const Color(0xFF283593);
-Color colorAccentLight = const Color(0xFF5C6BC0);
+Color colorAccentDark = const Color(0xFF013698);
+Color colorAccentLight = const Color(0xFF5f99d7);
