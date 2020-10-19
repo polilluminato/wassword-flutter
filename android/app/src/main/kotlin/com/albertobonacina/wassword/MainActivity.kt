44c4ee2c72d32e7cbbe8b4270c4b0ff57ff6c169
Alberto Bonacina <bonacina.alberto@gmail.com>
2020-10-19 08:10:28 +0200

    Fix some Android Files

--- android/app/src/main/kotlin/com/albertobonacina/wassword/MainActivity.kt
+++ android/app/src/main/kotlin/com/albertobonacina/wassword/MainActivity.kt
@@ -2,8 +2,5 @@ package com.albertobonacina.wassword
 
 import io.flutter.embedding.android.FlutterActivity
 
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
