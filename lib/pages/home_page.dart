280aa4676ff70bd76e621adaa28922956a1b942a
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-07-08 08:14:19 +0200

    feat: Add customization on the home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ import 'package:wassword/pages/about_page.dart';
 import 'package:wassword/stores/password.dart';
 import 'package:wassword/styles/my_colors.dart' as mColors;
 import 'package:wassword/styles/my_dimens.dart' as mDimens;
+import 'package:wassword/ui/action_button.dart';
 import 'package:wassword/ui/option_button.dart';
 
 final passwordStore = Password();
@@ -63,10 +64,10 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Container(
-        padding: EdgeInsets.all(16),
         child: Column(
           children: <Widget>[
             Container(
+              margin: EdgeInsets.all(24),
               height: 200,
               decoration: new BoxDecoration(
                 color: mColors.colorEnabled,
@@ -86,70 +87,118 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+            //https://medium.com/flutter-community/flutter-sliders-demystified-4b3ea65879c
             Observer(
-              builder: (_) => Slider(
-                min: 8.0,
-                max: 32.0,
-                activeColor: Colors.white,
-                divisions: 20,
-                value: passwordStore.numberCharPassword,
-                onChanged: (double value) {
-                  setState(
-                    () {
-                      passwordStore
-                        ..changeLenght(value)
-                        ..generateNew();
-                    },
-                  );
-                },
+              builder: (_) => SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  activeTrackColor: mColors.colorEnabled,
+                  trackHeight: mDimens.heightSlider,
+                  inactiveTrackColor: mColors.colorDisabled,
+                  thumbColor: mColors.colorEnabled,
+                  thumbShape: RoundSliderThumbShape(
+                      enabledThumbRadius: mDimens.heightSlider),
+                ),
+                child: Slider(
+                  min: 8.0,
+                  max: 32.0,
+                  divisions: 20,
+                  value: passwordStore.numberCharPassword,
+                  onChanged: (double value) {
+                    setState(
+                      () {
+                        passwordStore
+                          ..changeLenght(value)
+                          ..generateNew();
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                OptionButton(
-                  title: "Uppercase",
-                  description: "ABC",
-                  icon: Icons.arrow_back,
-                  active: false,
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                OptionButton(
-                  title: "Lowercase",
-                  description: "abc",
-                  icon: Icons.arrow_back,
-                  active: true,
-                ),
-              ],
             ),
             SizedBox(
               height: 16,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                OptionButton(
-                  title: "Numbers",
-                  description: "123",
-                  icon: Icons.arrow_back,
-                  active: true,
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                OptionButton(
-                  title: "Special",
-                  description: "@£*",
-                  icon: Icons.arrow_back,
-                  active: true,
-                ),
-              ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  OptionButton(
+                    title: "Uppercase",
+                    description: "ABC",
+                    icon: Icons.arrow_back,
+                    active: false,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  OptionButton(
+                    title: "Lowercase",
+                    description: "abc",
+                    icon: Icons.arrow_back,
+                    active: true,
+                  ),
+                ],
+              ),
             ),
-            Container(),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  OptionButton(
+                    title: "Numbers",
+                    description: "123",
+                    icon: Icons.arrow_back,
+                    active: true,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  OptionButton(
+                    title: "Special",
+                    description: "@£*",
+                    icon: Icons.arrow_back,
+                    active: true,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 64,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  ActionButton(
+                    text: "Copy",
+                    icon: Icons.copy,
+                    isMain: false,
+                    callback: () {},
+                  ),
+                  ActionButton(
+                    text: "Generate",
+                    icon: Icons.settings,
+                    isMain: true,
+                    callback: () {},
+                  ),
+                  ActionButton(
+                    text: "Share",
+                    icon: Icons.share,
+                    isMain: false,
+                    callback: () {},
+                  )
+                ],
+              ),
+            ),
             // Container(
             //   padding: EdgeInsets.only(top: 20, left: 12, right: 12),
             //   child: new Column(
