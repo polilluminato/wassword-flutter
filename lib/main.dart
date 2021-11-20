ddbce2c4167a3f414edd7fed416a1b5074c3edac
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-11-20 14:42:59 +0100

    feat: Update dependencies
    Update to Bloc v8

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,25 @@ import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wassword/app/app.dart';
 import 'package:wassword/app/app_bloc_observer.dart';
+import 'package:wassword/cubit/password_cubit.dart';
 import 'package:wassword/styles/my_colors.dart' as mColors;
 
 void main() {
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
-  Bloc.observer = AppBlocObserver();
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
 
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       systemNavigationBarColor: mColors.backgroundView, // navigation bar color
       statusBarColor: mColors.backgroundView, //top bar color
       statusBarIconBrightness: Brightness.light, // status bar icons' color
-      statusBarBrightness: Brightness.dark));
+      statusBarBrightness: Brightness.dark),);
+
+  BlocOverrides.runZoned(() {
+    PasswordCubit();
+  }, blocObserver: AppBlocObserver());
+
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
 
   runZonedGuarded(
     () => runApp(App()),
