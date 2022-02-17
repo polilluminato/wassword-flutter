929208782ac81715675567b4a969641b91b5a9e1
Alberto Bonacina <bonacina.alberto@gmail.com>
2022-02-17 23:01:10 +0100

    feat: Add new package to control the window size on desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,15 +10,31 @@ import 'package:wassword/app/app.dart';
 import 'package:wassword/app/app_bloc_observer.dart';
 import 'package:wassword/cubit/password_cubit.dart';
 import 'package:wassword/styles/my_colors.dart' as mColors;
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+  await windowManager.ensureInitialized();
 
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      systemNavigationBarColor: mColors.backgroundView, // navigation bar color
-      statusBarColor: mColors.backgroundView, //top bar color
-      statusBarIconBrightness: Brightness.light, // status bar icons' color
-      statusBarBrightness: Brightness.dark),);
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    windowManager.waitUntilReadyToShow().then((_) async {
+      // Hide window title bar
+      if (!Platform.isLinux) {
+        await windowManager.setTitleBarStyle('hidden');
+      }
+      await windowManager.setSize(const Size(385, 835));
+    });
+  }
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+        systemNavigationBarColor:
+            mColors.backgroundView, // navigation bar color
+        statusBarColor: mColors.backgroundView, //top bar color
+        statusBarIconBrightness: Brightness.light, // status bar icons' color
+        statusBarBrightness: Brightness.dark),
+  );
 
   BlocOverrides.runZoned(() {
     PasswordCubit();
