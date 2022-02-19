1c5aeabe9ca3bfd6af3413f7e4c9709770a0cf71
Alberto Bonacina <bonacina.alberto@gmail.com>
2022-02-19 09:08:36 +0100

    fix: Fix startup on web and crash on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,22 +10,26 @@ import 'package:wassword/app/app.dart';
 import 'package:wassword/app/app_bloc_observer.dart';
 import 'package:wassword/cubit/password_cubit.dart';
 import 'package:wassword/styles/colors.dart' as mColors;
-// import 'package:window_manager/window_manager.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
 
-  // await windowManager.ensureInitialized();
-  // if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-  //   windowManager.waitUntilReadyToShow().then((_) async {
-  //     // Hide window title bar
-  //     if (!Platform.isLinux) {
-  //       await windowManager.setTitleBarStyle('hidden');
-  //     }
-  //     await windowManager.setSize(const Size(385, 835));
-  //   });
-  // }
+  if (!kIsWeb &&
+      !Platform.isAndroid &&
+      !Platform.isIOS &&
+      (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      // Hide window title bar
+      if (!Platform.isLinux) {
+        await windowManager.setTitleBarStyle('hidden');
+      }
+      await windowManager.setSize(const Size(385, 835));
+    });
+  }
 
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
