78d785906d1a2d645c68f1cd9f53be9b366a2820
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-09-23 23:17:31 +0200

    chore: Upgrade dependencies

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,33 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'router.dart';
+import 'package:wassword/pages/about_page.dart';
+import 'package:wassword/pages/home/home_page.dart';
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
 
-  late final router = buildRouter();
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          SystemChrome.setSystemUIOverlayStyle(
+            SystemUiOverlayStyle(
+              systemNavigationBarColor: Theme.of(context).colorScheme.surface,
+            ),
+          );
+          return const HomePage();
+        },
+      ),
+      GoRoute(
+        path: '/about',
+        builder: (context, state) => const AboutPage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
