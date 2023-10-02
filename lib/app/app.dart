c44a764e3d0a60d61961d4f5626d544ea9df4472
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-10-02 21:14:54 +0200

    drop: Removed flex_color_scheme from dependencies and changed reference to app theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,6 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:wassword/pages/about_page.dart';
 import 'package:wassword/pages/home/home_page.dart';
 import 'package:wassword/provider/brightness_provider.dart';
@@ -27,32 +25,18 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final bProvider = ref.watch(brightnessProvider);
-
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
-      theme: bProvider == Brightness.light
-          ? FlexThemeData.light(
-              scheme: FlexScheme.flutterDash,
-              visualDensity: FlexColorScheme.comfortablePlatformDensity,
-              useMaterial3: true,
-              swapLegacyOnMaterial3: true,
-              textTheme: GoogleFonts.robotoTextTheme(
-                Theme.of(context).textTheme,
-              ),
-            )
-          : FlexThemeData.dark(
-              scheme: FlexScheme.flutterDash,
-              visualDensity: FlexColorScheme.comfortablePlatformDensity,
-              useMaterial3: true,
-              swapLegacyOnMaterial3: true,
-              textTheme: GoogleFonts.robotoTextTheme(
-                Theme.of(context).textTheme,
-              ),
-            ),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: ref.watch(brightnessProvider),
+        ),
+      ),
     );
   }
 }
