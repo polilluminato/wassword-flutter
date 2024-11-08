3c967fe7190ae2f6f28ff6befc89eebb3cf89104
Alberto Bonacina <alberto.bonacina@soluzione1.it>
2024-11-08 17:21:33 +0100

    add: Add first supporto for internationalisation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flex_seed_scheme/flex_seed_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wassword/pages/about_page.dart';
@@ -28,6 +29,8 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+extension LocalizedBuildContext on BuildContext {
+  AppLocalizations get loc => AppLocalizations.of(this);
+}
+
+extension Hardcoded on String {
+  String get hardcoded => this;
+}
