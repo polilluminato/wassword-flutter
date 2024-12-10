a5ae63ea4f3254d740b5968b09fe9811b8b93159
Alberto Bonacina <alberto.bonacina@soluzione1.it>
2024-12-10 14:58:25 +0100

    feat: Create method to Global access to localizations

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:wassword/pages/about_page.dart';
 import 'package:wassword/pages/home/home_page.dart';
 import 'package:wassword/pages/language_page.dart';
 
 part 'app_router.gr.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 @AutoRouterConfig(replaceInRouteName: 'Screen|Page,Route')
 class AppRouter extends RootStackRouter {
--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:wassword/app/app_router.dart';
+
+/*
+I created two methods to access localised strings:
+
+``dart
+// Before (with BuildContext)
+Text(context.loc.someString)
+
+// Now (without BuildContext)
+Text(l10n.someString)
+```
+
+This approach uses the `navigatorKey.currentContext` to access the context needed 
+for localisation. The l10n getter will throw an exception if used before 
+the MaterialApp is initialised or if there is no valid context available.
+
+Some important notes:
+* Be sure to use l10n only after the MaterialApp is fully initialised.
+* This approach works well for most cases, but keep in mind
+  that it does not automatically rebuild widgets when the locale changes.
+* For widgets that need to react to locale changes, 
+  you might want to use the BuildContext approach with an inherited widget.
+*/
+
+/// Global access to localizations
+AppLocalizations get l10n {
+  final context = navigatorKey.currentContext;
+  if (context == null) throw Exception('No context available for localization');
+  return AppLocalizations.of(context);
+}
 
 extension LocalizedBuildContext on BuildContext {
   AppLocalizations get loc => AppLocalizations.of(this);
